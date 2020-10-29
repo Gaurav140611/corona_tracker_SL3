eb8486d60e3c5c16849eb453b3f48b59f59ce127
Gaurav140611 <kumarsinghgaurav54@gmail.com>
2020-10-29 20:03:28 +0530

    Updates

---
+++ precache-manifest.7473ebf62afb28514da31bb87a3d324b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fbef51bca3d7633f220a8cb130baa7b",
+    "url": "/corona_tracker_SL3/index.html"
+  },
+  {
+    "revision": "bb32ff011416558ff4e3",
+    "url": "/corona_tracker_SL3/static/css/main.bbc865ad.chunk.css"
+  },
+  {
+    "revision": "7d956a5be27912c7570c",
+    "url": "/corona_tracker_SL3/static/js/2.11e88832.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona_tracker_SL3/static/js/2.11e88832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb32ff011416558ff4e3",
+    "url": "/corona_tracker_SL3/static/js/main.856d1327.chunk.js"
+  },
+  {
+    "revision": "bb4ca3784a05a2cd96ff",
+    "url": "/corona_tracker_SL3/static/js/runtime-main.f531b35c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona_tracker_SL3/static/media/image.d7265326.png"
+  }
+]);
